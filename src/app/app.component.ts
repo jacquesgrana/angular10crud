268940090bff5fba057be459f8a7b9ccd8f1485d
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-31 16:07:25 +0200

    ajout formulaire et barre de chargement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+} from '@angular/router';
+import { LoadingBarService } from '@ngx-loading-bar/core';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular10crud';
+
+  loader = this.loadingBar.useRef();
+
+  constructor(private loadingBar: LoadingBarService, private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.loader.start();
+    }
+
+    if (event instanceof NavigationEnd) {
+      this.loader.complete();
+    }
+
+    if (event instanceof NavigationCancel) {
+      this.loader.stop();
+    }
+
+    if (event instanceof NavigationError) {
+      this.loader.stop();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductGetComponent } from './product-get/product-get.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+//import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductGetComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
